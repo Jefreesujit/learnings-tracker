66359ab25209f44428e6bc8fdb810dd455f6584a
jefreesujit <e.jefree110@gmail.com>
2022-06-04 19:16:32 +0530

    Update about page with links and formatting

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Image, Text, View, Button, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Image, Text, Linking, ScrollView, Button, TouchableOpacity, StatusBar } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import { getLearningQuote } from '../utils';
 
 const Success = ({ route, navigation }) => {
@@ -16,34 +17,39 @@ const Success = ({ route, navigation }) => {
   const { quote, by } = getLearningQuote();
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor={'#80c905'} />
       <Text adjustsFontSizeToFit style={styles.welcomeTitle}>Learnings Today</Text>
       <Text adjustsFontSizeToFit style={styles.description}>
         Learnings Today is a simple tracker app that enables you to note down your everyday learnings and helps you to manage them.
         You also get a personalised learning timeline, where you can revisit your learnings and recall them.
       </Text>
-      <Text adjustsFontSizeToFit style={styles.description}>
-        This project is built during my free time, so incase if you face any issues or want to share any feedbacks, feel free to drop a message via the mail below
+      {/* <Text adjustsFontSizeToFit style={styles.description}>
+        The app is completely free to use and doesnt monetize in any way. (Not even ADs - Yes, totally free!!)
+        At the same time it takes time, effort and funds to maintain. If you like this work and want to contribute, you can support with the link below:
       </Text>
-      <Text adjustsFontSizeToFit style={styles.navLink}>
+      <Text style={styles.navLink} onPress={() => {
+          Linking.openURL('https://ko-fi.com/learningstoday');
+        }}>
+        Buy me a Coffee at ko-fi.com
+      </Text> */}
+      <Text adjustsFontSizeToFit style={styles.description}>
+        This project is built during my free time, so incase if you face any issues or want to share any feedbacks, feel free to drop a message to the email below:
+      </Text>
+      <Text adjustsFontSizeToFit style={styles.navLink} onPress={() => {
+        Linking.openURL('mailto:learningstodayapp@gmail.com');
+      }}>
         learningstodayapp@gmail.com
       </Text>
-      {/* <Text adjustsFontSizeToFit style={styles.description}>
-        Learnings Today is totally free to use and doesnt monetize in any way. (Not even ADs - Yes, totally free!!)
-        At the same time, it takes funds, time and effort to maintain. If you like this work and want to contribute, you can support with the link below
-        KO-FI
-      </Text>
-      <a href='https://ko-fi.com/learningstoday' target='_blank'>
-        <img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
-      </a> */}
       <Text adjustsFontSizeToFit style={styles.welcomeTitle}>
         Credits
       </Text>
       <Text adjustsFontSizeToFit style={styles.subHeader}>
         Developer
       </Text>
-      <Text adjustsFontSizeToFit style={styles.listItem}>
+      <Text adjustsFontSizeToFit style={styles.listItem} onPress={() => {
+          Linking.openURL('https:/github.com/jefreesujit');
+        }}>
         Jefree Sujit
       </Text>
       <Text adjustsFontSizeToFit style={styles.subHeader}>
@@ -52,7 +58,7 @@ const Success = ({ route, navigation }) => {
       <Text adjustsFontSizeToFit style={styles.listItem}>
         Aatish Daniel
       </Text>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -63,13 +69,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     // textAlign: 'center',
-    padding: 24,
+    padding: 16,
     width: '100%',
     // textAlign: 'justify',
   },
+  description: {
+    padding: 16,
+    paddingBottom: 8,
+    paddingTop: 8,
+    fontSize: 16,
+  },
+  subHeader: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    padding: 8,
+  },
+  listItem: {
+    color: '#80c905',
+    textDecorationLine: 'underline',
+    fontSize: 16,
+  },
   welcomeTitle: {
-    marginTop: 8,
-    marginBottom: 12,
+    margin: 8,
     fontSize: RFValue(22),
     alignItems: 'flex-start',
     textAlign: 'left',
@@ -88,10 +109,13 @@ const styles = StyleSheet.create({
     margin: 12,
     padding: 8,
     fontSize: RFValue(16),
-    textTransform: 'uppercase',
+    // textTransform: 'uppercase',
     fontWeight: 'bold',
     color: '#80c905',
-    backgroundColor: '#80c90528'
+    // backgroundColor: '#80c90528',
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: '#80c905',
   }
 });
 
